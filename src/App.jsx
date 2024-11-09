603e4d4aed1431c97131d8b61907b5745885a271
yuliana-alves <yulianaalves0101@gmail.com>
2024-11-08 21:40:42 -0300

    Co-authored-by: José Ignacio Festari Berón <festari@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Imagen from "./components/header";
+import data from "./data.json";
 
+function App() {
+  return (
+    <>
+      <Imagen />
+      <div className="container">
+        <div className="grilla">
+          {data.map((movie) => (
+            <div key={movie.id} className="tarjeta">
+              <img src={movie.poster_path} alt={movie.title} />
+              <h2>{movie.title}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
 
-
-export default App
+export default App;
---
+++ src/components/pelis.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import data from "../data.json";
+import "./Pelis.css";
+
+const Pelis = () => {
+  return (
+    <div className="grilla">
+      {data.map((movie) => (
+        <div key={movie.id} className="tarjeta">
+          <img src={movie.poster_path} alt={movie.title} />
+          <h2>{movie.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Pelis;
