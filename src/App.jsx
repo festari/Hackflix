8e4e8f10715aafe5c37cdc6b72434d17b869171d
MythCOS <ignaciofesatri115@gmail.com>
2024-11-12 20:10:30 -0300

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Imagen from "./components/header";
 import data from "./data.json";
-import Rating from "./components/stars";
-
 function App() {
   return (
     <>
