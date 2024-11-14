97f15d7f71b7432733d3226432df526d546b902f
yuliana-alves <yulianaalves0101@gmail.com>
2024-11-14 18:58:49 -0300

    cambios
    No mas slider, components con mayuscula

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Imagen from "./components/header";
-import Pelis from "./components/pelis";
-import Slider from "./components/slider";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Pelis from "./components/Pelis";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -21,7 +19,6 @@ function App() {
   return (
     <>
       <Imagen />
-      <Slider movies={movies} /> 
       <Pelis movies={movies} />
     </>
   );
