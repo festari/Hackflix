6d32fc56f954bc117d91bfadb65a56f07f93328b
NicolasHernandezAnima <nicolas.hernandez@anima.edu.uy>
2024-11-14 19:15:25 -0300

    estrellas
    Co-Authored-By: yuliana-alves <179268639+yuliana-alves@users.noreply.github.com>
    Co-Authored-By: José Ignacio Festari Berón <102311790+festari@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import "./App.css";
 import Imagen from "./components/header";
 import Pelis from "./components/pelis";
 import Slider from "./components/slider";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Stars from "./components/stars";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [rating, setRating] = useState(0);
 
   useEffect(() => {
     fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=c86d2f312ee79124783dcee4dc3d5cc0&include_adult=false&page=1&sort_by=popularity.desc&vote_count.gte=40"
+      "https://api.themoviedb.org/3/discover/movie?api_key=c86d2f312ee79124783dcee4dc3d5cc0&include_adult=false&page=2&sort_by=popularity.desc&vote_count.gte=40"
     )
       .then((response) => response.json())
       .then((data) => {
@@ -21,7 +23,8 @@ function App() {
   return (
     <>
       <Imagen />
-      <Slider movies={movies} /> 
+      <Slider movies={movies} />
+      <Stars rating={rating} setRating={setRating} />
       <Pelis movies={movies} />
     </>
   );
--- src/components/stars.jsx
+++ src/components/stars.jsx
@@ -1,1 +1,20 @@
+import ReactStars from "react-rating-stars-component";
 
+function Stars({ rating, setRating }) {
+  const ratingChanged = (newRating) => {
+    setRating(newRating); // borradme
+  };
+  return (
+    <>
+      <ReactStars
+        count={5}
+        onChange={ratingChanged}
+        size={24}
+        activeColor="#ffd700"
+      />
+      ,
+    </>
+  );
+}
+
+export default Stars;
