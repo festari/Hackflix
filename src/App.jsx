2bc8b4fd9ec0939c9eda7ded784e72100d9b38d9
Festari <ignaciofestari115@gmail.com>
2024-11-14 19:51:43 -0300

    Added Infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import "./App.css";
 import Imagen from "./components/header";
 import Pelis from "./components/Pelis";
@@ -8,10 +9,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [rating, setRating] = useState(0);
+  const [page, setPage] = useState(2); // Inicializa la página en 2
 
   useEffect(() => {
     fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=c86d2f312ee79124783dcee4dc3d5cc0&include_adult=false&page=2&sort_by=popularity.desc&vote_count.gte=40"
+      `https://api.themoviedb.org/3/discover/movie?api_key=c86d2f312ee79124783dcee4dc3d5cc0&include_adult=false&page=1&sort_by=popularity.desc&vote_count.gte=40`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -19,11 +21,34 @@ function App() {
       });
   }, []);
 
+  const fetchData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=c86d2f312ee79124783dcee4dc3d5cc0&include_adult=false&page=${page}&sort_by=popularity.desc&vote_count.gte=40`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies((prevMovies) => [...prevMovies, ...data.results]);
+        setPage((prevPage) => prevPage + 1);
+      });
+  };
+
   return (
     <>
       <Imagen />
       <Stars rating={rating} setRating={setRating} />
-      <Pelis movies={movies} />
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={fetchData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <Pelis movies={movies} />
+      </InfiniteScroll>
     </>
   );
 }
