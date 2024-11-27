20f498ebc3d81f1ca899cc9255ba133760fa1e6d
NicolasHernandezAnima <nicolas.hernandez@anima.edu.uy>
2024-11-26 21:33:55 -0300

    Okey cambié el algoritmo y ahora solo ordena las peliculas en "OurSelection"

--- src/utils/quicksort.js
+++
@@ -1,8 +0,0 @@
-export const quicksort = (movies) => {
-  if (movies.length <= 1) return movies;
-  const pivot = movies[0].title.toLowerCase();
-  const smaller = movies.filter((movie) => movie.title.toLowerCase() < pivot);
-  const greater = movies.filter((movie) => movie.title.toLowerCase() > pivot);
-  const equal = movies.filter((movie) => movie.title.toLowerCase() === pivot);
-  return [...quicksort(smaller), ...equal, ...quicksort(greater)];
-};
